43afc6d13ed674857c2caba1767020bfc11af1dc
Varushhhka <obzoginavarvara@gmail.com>
2022-04-03 13:50:16 +0300

    Add artists

---
+++ data/artists.py
@@ -0,0 +1,18 @@
+import sqlalchemy
+from sqlalchemy import orm
+from sqlalchemy_serializer import SerializerMixin
+
+from data.db_session import SqlAlchemyBase
+
+
+class Artists(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'artists'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    patronymic = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    initial_text = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
+
+    def __repr__(self):
+        return f'<Artist> {self.text}'
